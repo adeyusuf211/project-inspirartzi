c406d105e647c4f02f59a8bd103d9da933a7eb0b
adeyusuf211 <adeyusuf005@gmail.com>
2021-04-01 16:23:24 +0700

    Membuat loader

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ const nav = document.querySelector('.g-nav');
 window.addEventListener('scroll', function() {
     nav.classList.toggle('fixed', scrollY > 0);
 });
+
+const load = document.querySelector('.load');
+
+window.addEventListener('beforeunload', function() {
+    load.style.display = "flex";
+});
