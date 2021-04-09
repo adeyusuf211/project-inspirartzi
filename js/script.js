36aad3d7aaa2434863299310ea14384b32f41515
adeyusuf211 <adeyusuf005@gmail.com>
2021-04-09 14:23:43 +0700

    Menambahkan icon pada navbar ketika dibuka di perangkat mobile

--- js/script.js
+++ js/script.js
@@ -9,3 +9,22 @@ const load = document.querySelector('.load');
 window.addEventListener('beforeunload', function() {
     load.style.display = "flex";
 });
+
+// script for smooth scrolling
+const links = document.querySelectorAll(".g-nav-list a");
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
+// end script
