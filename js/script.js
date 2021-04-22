dff7f04292e113e0a7c228c4ef6028150d5075a1
adeyusuf211 <adeyusuf005@gmail.com>
2021-04-22 12:31:26 +0700

    Mengubah gambar pada section about

--- js/script.js
+++ js/script.js
@@ -1,55 +1,52 @@
+// script for fixed navbar
 const nav = document.querySelector('.g-nav');
-
 window.addEventListener('scroll', function() {
     nav.classList.toggle('fixed', scrollY > 0);
 });
+// end fixed navbar script
 
+// script for custom load
 const load = document.querySelector('.load');
-
 window.addEventListener('beforeunload', function() {
     load.style.display = "flex";
 });
+// end custom load script
 
 // script for smooth scrolling
 const links = document.querySelectorAll(".g-nav-list a");
-
 for (const link of links) {
   link.addEventListener("click", clickHandler);
 }
-
 function clickHandler(e) {
   e.preventDefault();
   const href = this.getAttribute("href");
   const offsetTop = document.querySelector(href).offsetTop;
-
   scroll({
     top: offsetTop,
     behavior: "smooth"
   });
 }
-// end script
+// end smooth scrolling script
 
+// script for modal box
 const modal     = document.getElementById('talk');
 const modalTalk = document.querySelector('.modal');
 const close     = document.getElementById('close');
 const span      = document.getElementById('span');
-
 modal.addEventListener('click', (e) => {
   modalTalk.style.display = "grid";
 });
-
 close.addEventListener('click', function(e) {
   modalTalk.style.display = "none";
 });
-
 span.addEventListener('click', function() {
   modalTalk.style.display = "none";
 });
+// end modal box script
 
 
 // script for darkmode
 const theme = document.querySelector('.theme');
-
 theme.addEventListener('click', function() {
   document.body.classList.toggle('dark');
   theme.classList.toggle('dark');
@@ -65,21 +62,17 @@ theme.addEventListener('click', function() {
     </svg>`;
   }
 });
-// end script
+// end darkmode script
 
-// script for custom cursor
 // script for custom cursor
 let mouse       = document.querySelector('.cursor');
 const h3        = document.querySelectorAll('h3');
 const h4        = document.querySelectorAll('h4');
-
 window.addEventListener('mousemove', cursor);
-
 function cursor(e) {
     mouse.style.top   = e.pageY + 'px';
     mouse.style.left  = e.pageX + 'px';
 }
-
 h3.forEach(h3 => {
     h3.addEventListener('mouseover', () => {
         mouse.classList.add('link-grow');
@@ -88,7 +81,6 @@ h3.forEach(h3 => {
         mouse.classList.remove('link-grow');
     });
 });
-
 h4.forEach(h4 => {
   h4.addEventListener('mouseover', () => {
       mouse.classList.add('link-grow');
