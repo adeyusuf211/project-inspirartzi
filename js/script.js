8622650c7ef1c8601053df6e214afb85247e620c
adeyusuf211 <adeyusuf005@gmail.com>
2021-05-07 08:39:35 +0700

    Menambahkan kategori pada section works

--- js/script.js
+++ js/script.js
@@ -68,6 +68,12 @@ function darkTheme() {
     const svg = document.querySelector('.theme svg');
     svg.style.color = "black";
 
+    const btn = document.querySelectorAll('.g-works .works-button button');
+    for(button of btn) {
+      button.style.color = "#fff";
+    }
+
+
     // change background color and color footer
     footer.style.backgroundColor = "rgb(226, 146, 54)";
     const footerText = document.querySelector('.g-footer .footer-text h3');
@@ -82,6 +88,11 @@ function darkTheme() {
     theme.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
     </svg>`;
+
+    const btn = document.querySelectorAll('.g-works .works-button button');
+    for(button of btn) {
+      button.style.color = "#000";
+    }
 
     // change background color and color of footer
     footer.style.backgroundColor = "rgb(255, 216, 171)";
@@ -137,3 +148,27 @@ menu.forEach(m => {
     });
 });
 // end active menu script
+
+// Script for tabs
+const btn = document.querySelectorAll('.g-works .works-button button');
+const img = document.querySelectorAll('.g-works .works-list .works-card');
+
+btn.forEach(item => {
+    item.addEventListener('click', () => {
+        for(let i = 0; i < btn.length; i++) {
+            btn[i].classList.remove('active');
+        }
+        item.classList.add('active');
+
+        // show image
+        img.forEach(show => {
+            show.style.display = 'none';
+            show.style.transition = 'all 0.5s linear';
+            let image = item.textContent.toLowerCase();
+            if(show.getAttribute('data-att') === image || image === 'all') {
+                show.style.display = "flex";
+            }
+        })
+    });
+})
+// End tabs
